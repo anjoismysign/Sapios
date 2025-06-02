82d18c4eeca5e88accb32494e57f3b9af3fd51a4
Luis Benavides Naranjo <lubensaico@gmail.com>
2025-06-02 12:07:44 -0600

    adds RoleType enum and updates User class to use RoleType for user roles

--- src/main/java/education/sapios/Sapios/entity/User.java
+++ src/main/java/education/sapios/Sapios/entity/User.java
@@ -23,12 +23,8 @@ public class User
     @Column(nullable=false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-    @JoinTable(
-            name="users_roles",
-            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
-            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
-    private List<Role> roles = new ArrayList<>();
+    @Column(nullable = false)
+    private RoleType role;
 
     public Long getId() {
         return id;
@@ -60,13 +56,5 @@ public class User
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
     }
 }
